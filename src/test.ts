815bb13f02c3a6b279bcaf9ba9bd232a1cd13794
Raj Jaiswal <rajjaiswalsaumya@gmail.com>
2021-11-28 14:16:16 +0530

    Prettier: lint fixes.

--- src/test.ts
+++ src/test.ts
@@ -2,14 +2,15 @@
 
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
   /* eslint-disable no-unused-vars */
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp,
+  ): {
     keys(): string[];
     <T>(id: string): T;
   };
@@ -17,11 +18,7 @@ declare const require: {
 };
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-  { teardown: { destroyAfterEach: true } },
-);
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), { teardown: { destroyAfterEach: true } });
 
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
